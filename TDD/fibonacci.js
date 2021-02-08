3fcab0b779641fac0bd658e5ceac3a6511750dd2
LizKamugisha <liz.kamugisha@gmail.com>
2021-02-09 01:28:39 +0300

    add corrections to fibonacci test first approach

--- TDD/fibonacci.js
+++ TDD/fibonacci.js
@@ -7,17 +7,24 @@ The Fibonacci Series is a summation of the previous two numbers in a sequence
 with the first two numbers being 0 and 1. It can be represented by the equation Fn = Fn-1 + Fn-2
 */
 
-// TRIAL 1 : Finding the nth Fibonacci number using a while loop
-/*
+// FUNCTION 1 : Finding the nth Fibonacci number using a while loop
+
 const nthFibonacci = (num) => {
 	// Unit 1
-	if (num === 1) return 0;
+	if (num === 0) return 0;
+	// Unit 2
+	if (num === 1) return 1;
+	// Unit 3
 	if (num === 2) return 1;
+	// Exception A
+	if (num < 0) return 'number must be greater than or equal to 0';
+	// Exception B
+	if (num > 100) return 'number should not be greater than 100';
+	// Unit 4
 	let num1 = 1;
 	let num2 = 1;
 	let sum;
 	let i = 2;
-	// Unit 2
 	while (i < num) {
 		sum = num1 + num2;
 		num1 = num2;
@@ -27,31 +34,39 @@ const nthFibonacci = (num) => {
 	return num2;
 };
 
-// Intantiating the function.
-console.log(`The nth number is ${nthFibonacci(1)}`);
+// Console Log Test
+// const nth = 2;
+// const fibNum = nthFibonacci(nth);
+// console.log(`Fib(${nth}) = ${fibNum}`);
 
 // Exporting the function
 module.exports = nthFibonacci;
-*/
 
-// TRIAL 2 : Fibonacci Series of nth element using recussion method
+// FUNCTION 2 : Fibonacci Series of nth element using recussion method
 // The Fibonacci Sequence Reffrence: [0, 1] 1, 2, 3, 5, 8, 13, 21, 34, 55...
 
-// eslint-disable-next-line no-var
-var fibonacciSeries = (n) => {
-	// Unit one.
-	if (n === 1) {
+const fibonacciSeries = (n) => {
+	// Unit 1
+	if (n === 0 || n === 1) {
 		return [0, 1];
 	}
-	// Unit two.
-	// eslint-disable-next-line vars-on-top
-	// const s = fibonacciSeries(n - 1);
-	// s.push(s[s.length - 1] + s[s.length - 2]);
-	// return s;
+	// Exception A
+	if (n < 0) {
+		return 'number must be greater than or equal to 0';
+	}
+	// Exception B
+	if (n > 100) {
+		return 'number should not be greater than 100';
+	}
+
+	// Unit 2
+	const f = fibonacciSeries(n - 1);
+	f.push(f[f.length - 1] + f[f.length - 2]);
+	return f;
 };
 
-// Intantiating the function
-console.log(fibonacciSeries(1));
+// Console Log Test
+// console.log(fibonacciSeries(2));
 
 // Exporting the function
 module.exports = fibonacciSeries;
--- TDD/test/fibonacciTest.js
+++ TDD/test/fibonacciTest.js
@@ -1,37 +1,76 @@
 /*
-Fibonacci Test First Approach
-------------------------------
+TEST FIRST APPROACH
+----------------------
 Programming Pair : Liz Kamugisha & Rino Kitimbo
 */
 
-/*
-// TRIAL 1
-// Importing Chai & fibonacci sequence
-const chai = require('chai');
-const { assert } = chai;
-const nthFibonacci = require('./fibonacci');
+// Import Chai library
+const { assert } = require('chai');
 
-describe('fibonacci test', () => {
+// FUNCTION 1: Fibonacci While Loop
+const nthFibonacci = require('../fibonacci');
+
+/* eslint-disable no-undef */
+describe('fibonacci while loop tests', () => {
 	// Unit 1 test
-	it('check if fibonacci position 1 returns 0', () => {
-		const result = nthFibonacci(1);
-		assert.equal(result, 0, 'true');
+	it('check if fib(0) returns 0 / [0,1]', () => {
+		// assert(nthFibonacci(0) === 0);
+		assert.deepEqual(nthFibonacci(0), [0, 1]);
+	});
+	// Unit 2 test
+	it('check if fib(1) returns 1 / [0,1]', () => {
+		// assert(nthFibonacci(1) === 1);
+		assert.deepEqual(nthFibonacci(1), [0, 1]);
+	});
+	// Unit 3 test
+	it('check if fib(2) returns 1 / [0,1,1]', () => {
+		// assert(nthFibonacci(2) === 1);
+		assert.deepEqual(nthFibonacci(2), [0, 1, 1]);
+	});
+	// Exception A test
+	it('check if num < 0 returns string', () => {
+		assert.isString(nthFibonacci(-1), 'throw statement');
+	});
+	// Exception B test
+	it('check if num > 100 returns string', () => {
+		assert.isString(nthFibonacci(101), 'throw statement');
+	});
+	// Unit 4 test
+	it('check if function calculates fibonacci of any number', () => {
+		// assert.equal(nthFibonacci(8), 21);
+		assert.deepEqual(nthFibonacci(8), [0, 1, 1, 2, 3, 5, 8, 13, 21]);
+	});
+	// Validation test
+	it('check if input is number / array', () => {
+		// assert.isNumber(nthFibonacci(10));
+		assert.isArray(nthFibonacci(10), []);
 	});
 });
-*/
 
-// TRIAL 2
-const chai = require('chai');
-
-const { assert } = chai;
-
+// FUNCTION 2: Fibonacci Recurssion Method
 const fibonacciSeries = require('../fibonacci');
-/* eslint-disable no-undef */
-describe('fibonacci', () => {
-	// Unit one test.
-	it('Check if value n = 1 returns [0,1]', () => {
-		const result = fibonacciSeries(1);
-		assert.isArray(result, [0, 1]);
-	// Unit two test.
+
+describe('fibonacci recurssion method tests', () => {
+	// Unit 1 test
+	it('Check if n = 0 or n = 1, return fibonacci sequence of [0,1]', () => {
+		assert.deepEqual(fibonacciSeries(0), [0, 1]);
+		assert.deepEqual(fibonacciSeries(1), [0, 1]);
+	});
+	// Exception A test
+	it('check if num < 0 returns string', () => {
+		assert.isString(fibonacciSeries(-2), 'throw statement');
+	});
+	// Exception B test
+	it('check if num > 100 returns string', () => {
+		assert.isString(fibonacciSeries(200), 'throw statement');
+	});
+	// Unit 2 test
+	it('check if function returns fibonacci series of any number between 0 < 100', () => {
+		assert.deepEqual(fibonacciSeries(3), [0, 1, 1, 2]);
+		assert.deepEqual(fibonacciSeries(5), [0, 1, 1, 2, 3, 5]);
+	});
+	// Validation test
+	it('Check if function with valid input returns array of fibonacci numbers', () => {
+		assert.isArray(fibonacciSeries(2), []);
 	});
 });
